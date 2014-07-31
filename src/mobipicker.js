17b6d97d9080efd213e837241f75498fdbb2ce94
mdgs <madeagus@gmail.com>
2014-07-30 22:53:44 -0400

    add support for select scroller

--- src/mobipicker.js
+++ src/mobipicker.js
@@ -7,70 +7,77 @@
 angular.module('hari', [])
 
 .directive('mobiPicker', ['$parse',
-  function($parse) {
+    function($parse) {
 
-    var directive = {
-      restrict: 'A',
+      var directive = {
+        restrict: 'A',
 
-      // update scope variable, component -> scope
-      link: function(scope, elm, attrs) {
+        var directive = {
+          restrict: 'A',
 
-        // default option
-        var options = {
-          preset: 'date',
-          theme: 'default',
-          mode: 'scroller',
-          display: 'modal',
-          animate: 'none',
+          // update scope variable, component -> scope
+          link: function(scope, elm, attrs) {
 
-          onSelect: function(valueText, inst) {
-            scope.$apply(function() {
-              var getter = $parse(attrs['mobiPicker']);
-              var setter = getter.assign;
+            // default option
+            var options = {
+              preset: 'date',
+              theme: 'default',
+              mode: 'scroller',
+              display: 'modal',
+              animate: 'none',
 
-              setter(scope, angular.copy(elm.mobiscroll('getDate')));
-            });
-          }
+              onSelect: function(valueText, inst) {
+                scope.$apply(function() {
+                  var getter = $parse(attrs['mobiPicker']);
+                  var setter = getter.assign;
+
+                  if (['date', 'time'].indexOf(inst.settings.preset) >= 0) {
+                    setter(scope, angular.copy(elm.mobiscroll('getDate')));
+                  } else if (['select'].indexOf(inst.settings.preset) >= 0) {
+                    setter(scope, angular.copy(elm.mobiscroll('getValue')));
+                  }
+                });
+              }
+            };
+
+            // prepare initialization object for scroller
+            var initOptS = attrs['mobiPickerOptions'] || '{}';
+            var initOpt = scope.$eval(initOptS);
+
+            angular.extend(options, initOpt);
+
+            // init scroller
+            // elm.mobiscroll()[options.preset](options);
+            elm.scroller(options);
+
+          },
+
+          // update picker component, scope -> component
+          controller: ['$scope', '$element', '$attrs',
+            function($scope, $element, $attrs) {
+
+              $scope.$watch(
+                // watched variable in scope object
+                function() {
+                  var getter = $parse($attrs['mobiPicker']);
+                  return getter($scope);
+                },
+                // action for change
+                function(newValue) {
+                  var inst = $element.mobiscroll('getInst');
+
+                  if ((newValue instanceof Date) && ['date', 'time'].indexOf(inst.settings.preset) >= 0) {
+                    $element.mobiscroll('setDate', newValue, true);
+                  } else if (['select'].indexOf(inst.settings.preset) >= 0) {
+                    $element.mobiscroll('setValue', [newValue], true);
+                  }
+                },
+                true
+              );
+            }
+          ]
         };
 
-        // prepare initialization object for scroller
-        var initOptS = attrs['mobiPickerOptions'] || '{}';
-        var initOpt = scope.$eval(initOptS);
-
-        angular.extend(options, initOpt);
-
-        // init scroller
-        // elm.mobiscroll()[options.preset](options);
-        elm.scroller(options);
-
-      },
-
-      // update picker component, scope -> component
-      controller: ['$scope', '$element', '$attrs',
-        function($scope, $element, $attrs) {
-
-          $scope.$watch(
-            // watched variable in scope object
-            function() {
-              var getter = $parse($attrs['mobiPicker']);
-              return getter($scope);
-            },
-            // action for change
-            function(newValue) {
-              if (!(newValue instanceof Date)) {
-                throw new Error("'" + $attrs['mobiPicker'] + "' must be a Date object");
-              }
-
-              var inst = $element.mobiscroll('getInst');
-              $element.mobiscroll('setDate', newValue);
-              $element.val(inst.val);
-            },
-            true
-          );
-        }
-      ]
-    };
-
-    return directive;
-  }
-]);
+        return directive;
+      }
+    ]);
